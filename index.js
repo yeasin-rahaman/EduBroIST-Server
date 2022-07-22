23a40e430a80e65ca5ac7dd904106bab904fc07c
Dev70 <Shanto9585>
2022-07-22 19:54:29 +0600

    update

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpepp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpepp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpepp.mongodb.net/?retryWrites=true&w=majority`
 
 // console.log('mogno:',uri);
 const client = new MongoClient(uri, {
